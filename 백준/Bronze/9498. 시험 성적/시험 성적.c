41f208e8587f5575b30423a72273fc4c4e4f4bda
gyogyo98 <gyo98@naver.com>
2025-04-01 11:14:13 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+	int A;
+	scanf("%d", &A);
+	if (A >=90) printf("A\n");
+	else if (A>=80) printf("B\n");
+	else if (A >= 70) printf("C\n");
+	else if (A >= 60) printf("D\n");
+	else printf("F\n");
+}
